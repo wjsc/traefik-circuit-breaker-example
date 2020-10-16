48e0f0a6b2138c7f59c1ce34f2961bf593f98ec3
Walter Scarano <walter.scarano@naranjax.com>
2020-10-16 16:47:14 -0300

    working

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const type = 'fail-random';
+const port = 3000;
+const status_ok = 200;
+const status_error = 500;
+
+http.createServer( (request, response) => {
+  const status = Math.random() < 0.6 ? status_ok : status_error;
+  response.writeHead(status , {'Content-Type': 'application/json'});
+  console.log(status, request.path);
+  response.write(JSON.stringify({ type }));
+  response.end();
+}).listen(port, 
+    () => console.log(`Server '${type}' listening on port ${port}`)
+);
